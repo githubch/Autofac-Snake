211319674907d3e9f317e1e02058edca10d85c03
githubch <944214536@qq.com>
2014-01-15 22:45:34 +0800

    [hcheng] #20140115 a simple demo about the autofac

--- Autofac-Snake/Program.cs
+++ Autofac-Snake/Program.cs
@@ -1,14 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Autofac;
 
 namespace Autofac_Snake
 {
     class Program
     {
+        private static IContainer Container { get; set; }
         static void Main(string[] args)
         {
+            var builder = new ContainerBuilder();
+            builder.RegisterType <ConsoleOutput>().As<IOutput>();
+            builder.RegisterType<TodayWriter>().As<IDateWriter>();
+            Container = builder.Build();
+            WriteDate();
+        }
+
+        private static void WriteDate()
+        {
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var writer = scope.Resolve<IDateWriter>();
+                writer.WriteDate();
+            }
         }
     }
 }
---
+++ Autofac-Snake/TodayWriter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Autofac_Snake
+{
+    public interface IOutput
+    {
+        void Write(string content);
+    }
+
+    public class ConsoleOutput : IOutput
+    {
+        public void Write(string content)
+        {
+            Console.WriteLine(content);
+        }
+    }
+
+    public interface IDateWriter
+    {
+        void WriteDate();
+    }
+
+    class TodayWriter : IDateWriter
+    {
+        private readonly IOutput output;
+        public TodayWriter(IOutput output)
+        {
+            this.output = output;
+        }
+
+        public void WriteDate()
+        {
+            output.Write(DateTime.Today.ToShortDateString());
+        }
+    }
+}
